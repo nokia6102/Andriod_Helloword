e26bb9e60941f8c2d6b10a4b6a216a001e46f5b5
Alexcheng <Alexchengm@gmail.com>
2014-12-01 14:58:30 +0800

    First commit.

---
+++ src/com/example/helloword/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloword;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	TextView tv;
+	Button btn;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView1);
+        tv.setText("123");
+        }
+}
